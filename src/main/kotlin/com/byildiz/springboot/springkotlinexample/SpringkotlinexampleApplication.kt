f0f5513ada05b618239c22867dbebfd3b8341003
Burak <burak.yildiz0@outlook.com>
2022-03-09 14:58:57 +0300

    Initial commit

---
+++ src/main/kotlin/com/byildiz/springboot/springkotlinexample/SpringkotlinexampleApplication.kt
@@ -0,0 +1,11 @@
+package com.byildiz.springboot.springkotlinexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringkotlinexampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringkotlinexampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/byildiz/springboot/springkotlinexample/SpringkotlinexampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.byildiz.springboot.springkotlinexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringkotlinexampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
